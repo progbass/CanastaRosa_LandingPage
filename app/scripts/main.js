54fe8a8e30d052741a62abaed543d2c7cf85f918
progbass <progbass@gmail.com>
2017-08-15 00:31:26 -0500

    Form Updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,309 @@
-import '../../node_modules/@fancyapps/fancybox';
+//import '../../node_modules/@fancyapps/fancybox';
+
+
+// MODAL FORM 
+var ModalWindow = function(_target){
+    const modal_fragment = document.createDocumentFragment();
+    const modal_container = document.createElement('div');
+    
+    const background = document.createElement('div');
+    background.className = 'background';
+    background.setAttribute('style', "position: fixed; width: 100%; height: 100%; background: rgba(0,0,0,.45)");
+    
+    let targetContent = document.querySelector(_target);
+    let content = targetContent.parentNode.removeChild(targetContent);
+    //content.setAttribute("style", "position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%)");
+    
+    modal_fragment.appendChild(background);
+    modal_fragment.appendChild( content );
+    modal_container.appendChild( modal_fragment );
+    modal_container.className = 'modalWindow';
+    //modal_container.setAttribute("style", "position: relative; width: 100%; height: 100%; background: rgba(0,0,0,.4); top: 0; left: 0; z-index: 100");
+    
+    
+    modal_container.addEventListener('click', function(e){
+        e.stopPropagation();
+        if(e.target.className === 'background'){
+            closeWindow();
+        }
+    });
+    
+    
+    const closeWindow = function(){
+        modal_container.style.display = 'none';
+        document.querySelector('#main_container').style.position = 'relative';
+    }
+    const openWindow = function(){
+        modal_container.style.display = 'block';
+        document.querySelector('#main_container').style.position = 'fixed';
+    }
+    
+    
+    document.body.appendChild(modal_container);
+    closeWindow();
+    
+    return{
+        openWindow,
+        closeWindow
+    }
+};
+
+// Ajax Object
+window.AJAXObject = function(_url, _query, _callback){
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            _callback(xmlhttp.responseText);
+        }
+        
+        //alert(xmlhttp.readyState);
+    }
+
+    xmlhttp.open("POST", _url, true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+    xmlhttp.send(_query);
+}
+
+
+
+// REGISTER FORM
+window.RegisterForm = (function(){
+    
+    let register_page = 0;
+    const register_container = document.querySelector("#registerForm");
+    const register_sliderContainer = register_container.querySelector(".slider_container");
+    const register_slider = register_sliderContainer.querySelector('.slider');
+    const register_form = document.querySelector("#register_form");
+    const register_submit = register_form.querySelector('input[type=submit]');
+    const register_nav = register_container.querySelector('a.nav');
+    const register_stepsContainer = register_container.querySelector('.register_step');
+    const register_fields = [
+        register_form.querySelector('input[name=name_txt]'),
+        register_form.querySelector('input[name=email_txt]'),
+        register_form.querySelector('input[name=zip_txt]'),
+        register_form.querySelector('input[name=phone_txt]')
+    ];
+    
+    const categoriesHolder = register_container.querySelector('.category_list');
+    const categories = [].slice.call(register_container.querySelectorAll('input[type=checkbox]'));
+    categories.forEach(function(option){
+        
+        if(option.value != ""){
+            const category = document.createElement('li');
+            category.innerHTML = option.parentNode.textContent;
+            category.setAttribute('data-category', option.value);
+            
+            category.addEventListener('click', function(){
+                this.classList.toggle('active');
+                
+                
+                const selectedCategories = [];
+                let catList = [].slice.call(categoriesHolder.querySelectorAll('li'));
+                for( let i = 0; i<catList.length; i++){
+
+                    if(catList[i].classList.contains('active')){
+                        if(categories[i].value === catList[i].getAttribute('data-category')){
+                            categories[i].checked = true;
+                            selectedCategories.push(categories[i]);
+                        }
+                    } else {
+                        categories[i].checked = false;
+                    }
+                };
+                
+                if(!selectedCategories.length){
+                      register_nav.style.visibility = 'hidden';
+                } else {
+                      register_nav.style.visibility = 'visible';
+                }
+            });
+            
+            categoriesHolder.appendChild(category);
+        }
+    });
+    
+    
+    register_nav.style.visibility = 'hidden';
+    register_nav.addEventListener('click', function(e){
+        e.preventDefault();
+        register_page = 1;
+        this.style.display = 'none';
+        register_submit.style.display = 'block';
+        validateForm();
+        moveSlider();
+    });
+    register_fields.forEach(function(_element){
+        _element.addEventListener('blur', function(){
+            validateForm()
+        });
+    });
+    
+    
+    
+    const moveSlider = function(){
+        // Select Container
+        const bullets = register_stepsContainer.querySelectorAll('span');
+        bullets.forEach(function(element, index){
+            element.classList.toggle('active', false);
+            
+            if(index == register_page){
+                element.classList.toggle('active', true);
+            }
+        });
+        
+        
+        // Move Slider
+        register_slider.style.left = (-register_slider.parentNode.offsetWidth*register_page)+'px';
+    }
+    
+    
+    
+    const validateForm = function(){
+        register_submit.style.visibility = 'hidden';
+        let isValid = false;
+        
+        for(let i = 0; i<register_fields.length; i++){
+            let regex;
+            switch(register_fields[i].getAttribute('name')){
+                case 'name_txt':
+                    regex = /^[a-zA-Z ]{2,30}$/;
+                     isValid = (regex.test(register_fields[i].value));
+                    break;
+                case 'email_txt':
+                    regex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
+                    isValid = (regex.test(register_fields[i].value));
+                    break;
+                case 'zip_txt':
+                    regex = /^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$/;
+                    isValid = (regex.test(register_fields[i].value));
+                    break;
+                case 'phone_txt':
+                    //regex = /^([1-9]\d{1}-\d{4}-\d{4}|[1-9]\d{2}-\d{3}-\d{4})$/;
+                    //isValid = (regex.test(register_fields[i].value));
+                    break;
+            }
+            
+            if(!isValid)
+                break;
+        };
+        
+        
+        if(isValid){
+            register_submit.style.visibility = 'visible';
+            return true;
+        }
+        
+        return false;
+    }
+    
+    const submit = function(){
+        // Validate Form first
+        if(!validateForm()){
+            return false;
+        }
+        
+        // Form Query
+        let query = '?';
+        
+        // Input Fields
+        register_fields.forEach(function(_element){
+            try{
+                query += _element.getAttribute('name')+'='+_element.value+'&';
+            } catch(e){}
+        });
+        
+        
+        // Categories List
+        let checkboxes = document.getElementsByName('category_sct[]');
+        let vals = "";
+        for (let i=0; i<checkboxes.length; i++){
+            if (checkboxes[i].checked){
+                vals += ","+checkboxes[i].value;
+            }
+        }
+        if (vals) vals = vals.substring(1);
+        query += `category_sct=${vals}`;
+        
+        
+        ///
+        AJAXObject(register_form.getAttribute('action'), query);
+        return false;
+    }
+    
+    
+    const formSuccess = function(_response){
+        alert(_response); // Here is the response
+    }
+    
+    
+    
+    
+    validateForm();
+    return {
+        'submit': submit
+    }
+}());
+
+
+
+
+// REGISTER FORM
+window.InviteForm = (function(){
+      
+    const invite_container = document.querySelector("#mailForm");
+    const invite_form = document.querySelector("#invite_form");
+    const invite_submit = invite_form.querySelector('input[type=submit]');
+    const invite_fields = [
+        invite_form.querySelector('input[name=email_txt]'),
+        invite_form.querySelector('textarea[name=message_txt]')
+    ];
+    
+    
+    const validateForm = function(){
+        let query = '?';
+        invite_fields.forEach(function(_element){
+            try{
+                query += _element.getAttribute('name')+'='+_element.value+'&';
+            } catch(e){}
+        });
+        alert(query)
+        AJAXObject(invite_form.getAttribute('action'), query);
+        
+        return false;
+    }
+    
+    
+    const formSuccess = function(_response){
+        alert(_response); // Here is the response
+    }
+    
+    
+    return {
+        'submit': validateForm
+    }
+}());
+
+
+window.SocialShare = function(_network, _url, _title){
+    
+    let url = _url, apiEndpoint = "";
+    switch(_network){
+        case"facebook":
+            apiEndpoint = "https://www.facebook.com/sharer/sharer.php?u="+url;
+            break;
+            
+        case"twitter":
+            const title = encodeURIComponent(_title);
+            apiEndpoint = "https://twitter.com/intent/tweet?text="+title+"&url="+url
+    }
+    var f=500,g=250,h=screen.width/2-f/2,i=screen.height/2-g/2;    var myWindow = window.open(apiEndpoint,"win_share","width="+f+",height="+g+", top="+i+", left="+h);
+
+}
+
+window.SocialShare2 = function(_network, _url, _title){}
+
+
 
 (function(){
 	// Extend Image Prototype Functionality
@@ -34,111 +339,12 @@ import '../../node_modules/@fancyapps/fancybox';
 			image.resize();
 		});
 	});
+    
+    
+    
+    //
+    window.registerWindow = new ModalWindow("#registerForm");
+    window.shareWindow = new ModalWindow("#mailForm");
 }());
 
 
-
-// Ajax Object
-window.AJAXObject = function(_url, _query, _callback){
-    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            _callback(xmlhttp.responseText);
-        }
-        
-        //alert(xmlhttp.readyState);
-    }
-
-    xmlhttp.open("POST", _url, true);
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded")
-    xmlhttp.send(_query);
-}
-
-
-
-// REGISTER FORM
-window.RegisterForm = (function(){
-      
-    const register_container = document.querySelector("#registerForm");
-    const register_slider = register_container.querySelector(".slider_container");
-    const register_form = document.querySelector("#register_form");
-    const register_submit = register_form.querySelector('input[type=submit]');
-    const register_nav = register_container.querySelector('a.nav');
-    const register_fields = [
-        register_form.querySelector('input[name=category_sct]'),
-        register_form.querySelector('input[name=name_txt]'),
-        register_form.querySelector('input[name=email_txt]'),
-        register_form.querySelector('input[name=zip_txt]'),
-    ];
-    
-    
-    register_nav.addEventListener('click', function(e){
-        e.preventDefault();
-        this.style.display = 'none';
-        register_submit.style.display = 'block';
-        register_slider.style.left = (-register_slider.parentNode.offsetWidth)+'px';
-    });
-    
-    const validateForm = function(){
-        let query = '?';
-        register_fields.forEach(function(_element){
-            try{
-                query += _element.getAttribute('name')+'='+_element.value+'&';
-            } catch(e){}
-        });
-        
-        AJAXObject(register_form.getAttribute('action'), query);
-        
-        return false;
-    }
-    
-    
-    const formSuccess = function(_response){
-        alert(_response); // Here is the response
-    }
-    
-    
-    return {
-        'submit': validateForm
-    }
-}());
-
-
-
-
-// REGISTER FORM
-window.InviteForm = (function(){
-      
-    const invite_container = document.querySelector("#mailForm");
-    const invite_form = document.querySelector("#invite_form");
-    const invite_submit = invite_form.querySelector('input[type=submit]');
-    const invite_fields = [
-        invite_form.querySelector('input[name=name_txt]'),
-        invite_form.querySelector('textarea[name=message_txt]')
-    ];
-    
-    
-    const validateForm = function(){
-        let query = '?';
-        invite_fields.forEach(function(_element){
-            try{
-                query += _element.getAttribute('name')+'='+_element.value+'&';
-            } catch(e){}
-        });
-        alert(query)
-        AJAXObject(invite_form.getAttribute('action'), query);
-        
-        return false;
-    }
-    
-    
-    const formSuccess = function(_response){
-        alert(_response); // Here is the response
-    }
-    
-    
-    return {
-        'submit': validateForm
-    }
-}());
--- gulpfile.js
+++ gulpfile.js
@@ -1,187 +1,138 @@
-
-const browserify = require('browserify');
-const babelify = require('babelify')
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const gulp = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
-const browserSync = require('browser-sync');
+'use strict';
 const del = require('del');
-const wiredep = require('wiredep').stream;
+const browserify = require('browserify');
+const gulp = require('gulp');
+const rename = require("gulp-rename");
+const sass = require('gulp-sass');
+const babelify = require('babelify');
+const uglify = require('gulp-uglify');
+const source = require("vinyl-source-stream");
+const buffer = require('vinyl-buffer');
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const envify = require('envify');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpCopy = require('gulp-copy');
 
-const $ = gulpLoadPlugins();
-const reload = browserSync.reload;
 
-gulp.task('styles', () => {
-  return gulp.src('app/styles/*.scss')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
-});
-
-gulp.task('scripts', () => {
-  var b = browserify({
-    entries: 'app/scripts/main.js',
-    debug: true
-  })
-  .transform(babelify, {});
-
-  return b.bundle()
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel({
-        presets: ['es2015']
+gulp.task('styles', function () {
+  return gulp.src('./app/styles/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe(cleanCSS({
+      level: {
+        1: {
+          specialComments: 0
+        }
+      }
     }))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe(reload({stream: true}));
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(browserSync.stream());
 });
 
-function lint(files, options) {
-  return gulp.src(files)
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.eslint(options))
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-}
 
-gulp.task('lint', () => {
-  return lint('app/scripts/**/*.js', {
-    fix: true
-  })
-    .pipe(gulp.dest('app/scripts'));
-});
-gulp.task('lint:test', () => {
-  return lint('test/spec/**/*.js', {
-    fix: true,
-    env: {
-      mocha: true
-    }
-  })
-    .pipe(gulp.dest('test/spec/**/*.js'));
+gulp.task("js", function () {
+  // set up the browserify instance on a task basis
+    return browserify({
+      entries: './app/scripts/main.js',
+      transform: [envify, babelify],
+      //debug: true
+    })
+    .bundle()
+    .pipe(source("./app/scripts/main.js"))
+    .pipe(plumber())
+    .pipe(rename('main.js'))
+    .pipe(buffer())
+    /*.pipe(uglify({
+      mangle: false,
+      compress: {
+        //sequences: true,
+        //dead_code: true,
+        //conditionals: true,
+        //booleans: true,
+        //unused: true,
+        //if_return: true,
+        //join_vars: true
+        //drop_console: true 
+      }
+    }))*/
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./dist/scripts"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('html', ['styles', 'scripts'], () => {
+gulp.task('html', function () {
   return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 });
+
+
+
 
 gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
+  return gulp.src('./app/images/**/*')
+    .pipe(cache(imagemin({
       progressive: true,
       interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
+      optimizationLevel: 5,
       svgoPlugins: [{cleanupIDs: false}]
     })))
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest('./dist/images'))
+    .pipe(browserSync.stream());
 });
+
 
 gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe(gulp.dest('.tmp/fonts'))
-    .pipe(gulp.dest('dist/fonts'));
+    return gulp.src([
+      './app/fonts/**/*'
+    ])
+    .pipe(gulp.dest('./dist/fonts'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('extras', () => {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
+
+
+gulp.task('extras', () => {});
+
+
+gulp.task('clean', del.bind(null, ['dist']));
+
+
+gulp.task('set-dev-node-env', function() {
+    return process.env.NODE_ENV = 'development';
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('set-prod-node-env', function() {
+    return process.env.NODE_ENV = 'production';
+});
 
-gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
+
+gulp.task("serve", ['set-dev-node-env', 'html', "styles", "js", 'images', 'fonts', 'extras'], () => {
+  browserSync.init({
     server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
+      baseDir: "./dist/"
+      //https: true
     }
   });
 
-  gulp.watch([
-    'app/*.html',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', reload);
-
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
+  gulp.watch('./app/images/**/*', ['images']);
+  gulp.watch('./app/fonts/**/*', ['fonts']);
+  gulp.watch('./app/*.html', ['html']);
+  gulp.watch('./app/scripts/**/*.js', ['js']);
+  gulp.watch('./app/styles/**/*.scss', ['styles']);  
 });
 
-gulp.task('serve:dist', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist']
-    }
-  });
+gulp.task('build', ['set-prod-node-env', 'html', 'styles', 'js', 'images', 'fonts', 'extras'], () => {
+  //return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
-
-gulp.task('serve:test', ['scripts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
-});
-
-// inject bower components
-gulp.task('wiredep', () => {
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
-    .pipe(gulp.dest('app/styles'));
-
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('build', ['html', 'images', 'fonts', 'extras'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
-
 gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
